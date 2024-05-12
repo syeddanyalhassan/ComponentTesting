7a78ff880470ca5d6e40accb5da774531157f1ee
syeddanyalhassan-10P <danyal.hassan@10pearls.com>
2024-05-12 22:20:03 +0500

    new files added

---
+++ Angularcomp/cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+      
+    },
+    specPattern: "**/*.cy.ts",
+ 
+   
+  },
+});
